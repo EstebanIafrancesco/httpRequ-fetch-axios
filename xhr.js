5796c7ed80bb50e3117b64d558cd63b1b4549072
Esteban Iafrancesco <eiafrancescoa@dane.gov.co>
2021-05-04 18:05:57 -0500

    Http request GET method

---
+++ xhr.js
@@ -0,0 +1,32 @@
+const getBtn = document.getElementById("get-btn");
+const postBtn = document.getElementById("post-btn");
+
+const sendHttpRequest = (method, url) => {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+
+    xhr.responseType = "json";
+
+    xhr.onload = () => {
+      resolve(xhr.response);
+    };
+    xhr.send();
+  });
+  return promise;
+  
+};
+
+const getData = () => {
+  sendHttpRequest("GET", 'https://reqres.in/api/users').then(responseData =>{
+      console.log(responseData);
+  });
+};
+const sendData = () => {
+    sendHttpRequest('POST', 'https://reqres.in/api/register',{email: 'tester@com'}).then(responseData =>{
+        console.log(responseData);
+    })
+};
+
+getBtn.addEventListener("click", getData);
+postBtn.addEventListener("click", sendData);
